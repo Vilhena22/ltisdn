37dd250d035c692b1f1a4241a00d2e61593dea4f
Francisco Vilhena <2231800@my.ipleiria.pt>
2026-03-27 14:41:39 +0000

    Add and edits functionalities for DHCP

--- src/main/java/ApiClient/MikrotikConfig.java
+++ src/main/java/ApiClient/MikrotikConfig.java
@@ -2,7 +2,7 @@ package ApiClient;
 
 // MikrotikConfig.java
 public class MikrotikConfig {
-    public static final String baseUrl =  "https://192.168.56.103/rest"; //"https://192.168.79.1/rest";
+    public static final String baseUrl =  "https://192.168.79.1/rest";
     public static final String username = "admin";
     public static final String password = "microtik";
 }
